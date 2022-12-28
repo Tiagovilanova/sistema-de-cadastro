dc306f379a3561479eaeb5ddb4dcf75eddad60b9
vilatico <75148886+vilatico@users.noreply.github.com>
2022-12-28 16:00:09 -0300

    Console log indicando medificacoes do paulo

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 Console.log("Rodando o sistema de gerenciamento de jogos")
 Console.log("aplicação está se conectando ao banco de dados")
+//mais um console de log de teste:
+console.log("O paulo fez esse commit sem voce ver na aula")
